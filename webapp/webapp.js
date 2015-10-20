d1275765bd454bbe72fe7b58ec9b15f6d818ec0e
Natalie Akam <natalie.akam@gmail.com>
2015-10-20 23:31:59 +0100

    Added routes

--- webapp/webapp.js
+++ webapp/webapp.js
@@ -7,7 +7,23 @@ console.log('Server has started');
 function onRequest(req, res) {
   var pathName = url.parse(req.url).pathname;
   console.log(pathName);
-  res.writeHead(200);
-  res.write('Hello world');
-  res.end();
+  showPage(res, pathName);
+}
+
+function showPage(res, pathName) {
+  if(contentMap[pathName]) {
+    res.writeHead(200, {'Content-Type' : 'text/html'});
+    res.write(contentMap[pathName]);
+    res.end();
+  }else {
+    res.writeHead(404, {'Content-Type' : 'text/html'});
+    res.write('404 page not found');
+    res.end();
+  }
+}
+
+var contentMap = {
+  '/' : '<h1>Main Page</h1>',
+  '/contact' : '<h1>Contact</h1>',
+  '/about' : '<h1>About</h1>'
 }
